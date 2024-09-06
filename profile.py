25c7144007acf67f32a97935c4fcbc79e056a0d2
Dustin Maas <dmaas@flux.utah.edu>
2024-09-05 22:49:24 -0600

    Update docs. Add RIC option and overrides.

--- profile.py
+++ profile.py
@@ -13,7 +13,9 @@ tourDescription = """
 
 ### srsRAN 5G with Open5GS and Simulated RF
 
-This profile instantiates a single-node experiment for running and end to end 5G network using srsRAN_Project 23.5 (gNodeB), srsRAN_4G (UE), and Open5GS with IQ samples passed via ZMQ between the gNodeB and the UE. It requires a single Dell d430 compute node.
+This profile instantiates a single-node experiment for running and end to end 5G network using srsRAN_Project 24.04 (gNodeB), srsRAN_4G (UE), and Open5GS (in container) with IQ samples passed via ZMQ between the gNodeB and the UE. It requires a single Dell d430 compute node.
+
+Optionally, you deploy a containerized ORAN SC RIC and xApp on the same node to demonstrate a 5G RAN Intelligent Controller (RIC) and xApp interacting with the gNodeB.
 
 """
 tourInstructions = """
@@ -30,65 +32,79 @@ In an SSH session on `node`:
 # create a network namespace for the UE
 sudo ip netns add ue1
 
-# start tailing the Open5GS AMF log
-tail -f /var/log/open5gs/amf.log
+# start the Open5GS container
+cd /opt/srsRAN_Project/docker
+sudo docker compose up
 ```
 
-In a second session:
+"""
+
+if params.deployric:
+    tourDescription += \
+    """
+In another session
 
 ```
-# use tshark to monitor 5G core network function traffic
-sudo tshark -i lo \
-  -f "not arp and not port 53 and not host archive.ubuntu.com and not host security.ubuntu.com and not tcp" \
-  -Y "s1ap || gtpv2 || pfcp || diameter || gtp || ngap || http2.data.data || http2.headers"
+# start the ORAN SC RIC container
+cd /opt/oran-sc-ric
+sudo docker compose up
 ```
 
-In a third session:
+    """
+else:
+    tourDescription += \
+    """
+In another session:
 
 ```
 # start the gNodeB
-sudo gnb -c /etc/srsran/gnb.conf
+sudo /opt/srsRAN_Project/build/apps/gnb/gnb -c /etc/srsran/gnb.conf
 ```
 
-The AMF should show a connection from the gNodeB via the N2 interface and `tshark` will show NG setup/response messages.
+The AMF logs in the 5gc container should show a connection from the gNodeB via the N2 interface.
 
 In a forth session:
 
 ```
 # start the UE
-sudo srsue
+sudo /opt/srsRAN_4G/build/srsue/src/srsue /etc/srsran/ue.conf
 ```
 
-As the UE attaches to the network, the AMF log and gNodeB process will show progress and you will see NGAP/NAS traffic in the output from `tshark` as a PDU session for the UE is eventually established.
+As the UE attaches to the network, the AMF log and gNodeB process will show progress as a PDU session for the UE is eventually established.
 
 At this point, you should be able to pass traffic across the network via the previously created namespace in yet another session on the same node:
 
 ```
 # start pinging the Open5GS data network
-sudo ip netns exec ue1 ping 10.45.0.1
+sudo ip netns exec ue1 ping -i 0.1 10.45.1.1
 ```
 
-You can also use `iperf3` to generate traffic. E.g., for downlink, in one session:
+    """
+
+if params.deployric:
+    tourDescription += \
+    """
+In another session:
 
 ```
-# start iperf3 server for UE
-sudo ip netns exec ue1 iperf3 -s
+# start the xApp
+cd /opt/oran-sc-ric
+sudo docker compose exec python_xapp_runner ./kpm_mon_xapp.py --metrics=DRB.UEThpDl,DRB.UEThpUl --kpm_report_style=5
 ```
 
-And in another:
+You should see action in the RIC container logs as the KPM monitor xApp recieves metrics from the gNodeB and UE.
 
-```
-# start iperf3 client for CN data network
-sudo iperf3 -c {ip of UE (indicated in srsue stdout)}
-```
+    """
 
+tourDescription += \
+    """
 Note: When ZMQ is used by srsRAN to pass IQ samples, if you restart either of the `gnb` or `srsue` processes, you must restart the other as well.
 
 You can find more information about the open source 5G software used in this profile at:
 
 https://open5gs.org
 https://github.com/srsran/srsRAN_Project
-"""
+    """
 
 
 HEAD_CMD = "sudo -u `geni-get user_urn | cut -f4 -d+` -Hi /bin/sh -c 'EMULAB_ANSIBLE_NOAUTO=1 /local/repository/emulab-ansible-bootstrap/head.sh >/local/logs/setup.log 2>&1'"
@@ -109,6 +125,12 @@ node_types = [
 ]
 
 pc.defineParameter(
+    name="deployric",
+    description="Deploy ORAN SC RIC and xApp on the same node.",
+    typ=portal.ParameterType.BOOLEAN,
+    defaultValue=False,
+)
+pc.defineParameter(
     name="nodetype",
     description="Type of compute node to used.",
     typ=portal.ParameterType.STRING,
@@ -128,6 +150,10 @@ request.addRole(
     )
 )
 request.addOverride(Override("srsran_project_build_5gc", value="true"))
+
+if params.deployric:
+    request.addOverride(Override("srsran_project_enable_du_e2", value="true"))
+    request.addOverride(Override("srsran_project_e2sm_kpm_enabled", value="true"))
 
 node = request.RawPC("node")
 node.hardware_type = params.nodetype
